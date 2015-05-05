c15085948e4be7e76ad4b59f52e87c53eab523f7
De'Yonte W <dw19412@gmail.com>
2015-05-05 19:30:36 -0400

    added the method to update the record in Caspio using the PK_ID

--- src/Caspio.php
+++ src/Caspio.php
@@ -7,6 +7,7 @@ use \Httpful\Request as Request;
 class Caspio
 {
     public $access_token;
+    public $response_format_header = 'application/json';
 
     public function __construct($access_token)
     {   
@@ -22,6 +23,22 @@ class Caspio
         }
     }
 
+    public function setReponseFormat($response_type)
+    {
+        $response_type = strtolower($response_type);
+        switch ($response_type) {
+            case 'xml': 
+                $this->response_format_header = 'application/xml'
+                break;
+            case 'json':
+                $this->response_format_header = 'application/json';
+                break;
+            default:
+                $this->response_format_header = 'application/json';
+                break;
+        }
+    }
+
     public function testAPIAccessToken($access_token = '')
     {
         $this->access_token = $this->token_class->testAccessToken($access_token);
@@ -32,6 +49,30 @@ class Caspio
         $url = sprintf($rest_url.$table.'/rows?q=%s', urlencode(json_encode(array('where'=>'PK_ID = '.$id))));
         $response = Request::get($url)
             ->addHeader('Authorization','Bearer '.$this->access_token)
+            ->addHeader('Accept', $this->response_format_header)
+            ->send();
+
+        return $response;
+    }
+
+    public function updateRowByID($rest_url, $table, $id, $fields)
+    {
+        $url = sprintf($rest_url.$table.'/rows?q=%s', urlencode(json_encode(array('where'=>'PK_ID = '.$id))));
+        $response = Request::put($url)
+            ->addHeader('Authorization','Bearer '.$this->access_token)
+            ->addHeader('Accept', $this->response_format_header)
+            ->body(json_encode($fields))
+            ->send();
+
+        return $response;
+    }
+
+    public function deleteRowByID($rest_url, $table, $id)
+    {
+        $url = sprintf($rest_url.$table.'/rows?q=%s', urlencode(json_encode(array('where'=>'PK_ID = '.$id))));
+        $response = Request::delete($url)
+            ->addHeader('Authorization','Bearer '.$this->access_token)
+            ->addHeader('Accept', $this->response_format_header)
             ->send();
 
         return $response;
