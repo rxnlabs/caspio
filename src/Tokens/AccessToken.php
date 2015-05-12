3845a770a6c9c2a1f55e06c86ff51ce77fa86f64
De'Yonte Wilkinson <dwilkinson@agorafinancial.com>
2015-05-12 12:32:55 -0400

    fixed issue with the Access token script and refreshing the access token using the refresh token. In order to create a new access token, you must pass the client secret and client ID in a base64 encoded string as a Authorization header (which defeats the purpose of a refresh token if I have to keep passing the encoded client ID and client secret)

--- src/Tokens/AccessToken.php
+++ src/Tokens/AccessToken.php
@@ -35,11 +35,16 @@ class AccessToken
             return $error->get_error_code();
         }
 
-        $body = array('grant_type'=>'client_credentials','client_id'=>$client_id,'client_secret'=>$client_secret);
+        $body = array('grant_type'=>'client_credentials');
+        $encode_creds = base64_encode(sprintf('%1$s:%2$s', $client_id, $client_secret));
         $response = Request::post($oauth_path)
+            ->addHeader('Content-Type','application/x-www-form-urlencoded')
+            ->addHeader('Authorization','Basic '.$encode_creds)
             ->body(http_build_query($body))
             ->send();
         if ($response->code == 200) {
+            $this->access_token = $response->body->access_token;
+            $this->refresh_token = $response->body->refresh_token;
             return $response->body;
         } else {
             $error = new Error(404, 'Missing arguments $client_id, $client_secret, $oauth_path');
@@ -47,7 +52,7 @@ class AccessToken
         }
     }
 
-    public function renewAccessToken($refresh_url = '', $refresh_token = '')
+    public function renewAccessToken($refresh_url = '', $client_id = '', $client_secret = '', $refresh_token = '')
     {
         if (empty($refresh_url)) {
             $refresh_url = $this->$oauth_path;
@@ -57,32 +62,40 @@ class AccessToken
             $refresh_token = $this->refresh_token;
         }
 
-        if (empty($refresh_token) || empty($refresh_url)) {
-            $error = new Error(404, 'Missing arguments $refresh_token and $refresh_url');
+        if (empty($client_id)) {
+            $client_id = $this->client_id;
+        }
+
+        if (empty($client_secret)) {
+            $client_secret = $this->client_secret;
+        }
+
+        if (empty($refresh_token) || empty($refresh_url) || empty($client_id) || empty($client_secret)) {
+            $error = new Error(404, 'Missing arguments $refresh_url, $client_id, $client_secret or $refresh_token');
             return $error->get_error_code();
         }
 
         $body = array('grant_type'=>'refresh_token','refresh_token'=>$refresh_token);
+        $encode_creds = base64_encode(sprintf('%1$s:%2$s', $client_id, $client_secret));
         $response = Request::post($refresh_url)
+            ->addHeader('Content-Type','application/x-www-form-urlencoded')
+            ->addHeader('Authorization','Basic '.$encode_creds)
             ->body(http_build_query($body))
             ->send();
 
         if ($response->code == 200) {
-            $this->setAccessToken($response->body->access_token);
-            return $response->body->access_token;
+            $this->access_token = $response->body->access_token;
+            $this->refresh_token = $response->body->refresh_token;
+            return $response->body;
         } else {
             return false;
         }
     }
 
-    public function isAccessTokenValid($tables_url = '', $access_token = '')
+    public function isAccessTokenValid($tables_url, $access_token = '')
     {
         if (empty($access_token)) {
             $access_token = $this->access_token;
-        }
-
-        if (empty($tables_url)) {
-            $tables_url = $this->caspio_tables_url;
         }
 
         if (empty($access_token) || empty($tables_url)) {
